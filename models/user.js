b8a27ec199b07026358696f3ad9924e6115d4ec8
Iana Kozma <yana.kozma@gmail.com>
2023-06-26 10:38:29 +0200

    added hw05

--- models/user.js
+++ models/user.js
@@ -22,6 +22,7 @@ const userSchema = new Schema(
       enum: subscriptionList,
       default: 'starter',
     },
+    avatarURL: String,
     token: String,
   },
   { versionKey: false, timestamps: true }
